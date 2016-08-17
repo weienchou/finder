f5faa8ade5306aa50d6e87d5f1d6c8f5f3153914
weienchou <wayne@WAYNEs-MBPR.local>
2016-08-17 16:09:07 +0800

    fixed utf-8 encode

--- class_finder.php
+++ class_finder.php
@@ -11,6 +11,7 @@ class Finder {
 	var $stop_time = '';
 
 	function __construct($type) {
+		header("Content-Type:text/html; charset=utf-8");
 		ini_set('max_execution_time', 0);
 		ini_set('memory_limit', '-1');
 		set_time_limit(900);
